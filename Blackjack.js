abf364086dd4a91b99c6f8c7e0c9a3e82a3c4470
Evansii <vincent.casamayou@laposte.net>
2018-12-12 10:05:58 +0100

    blackjackv1

--- Blackjack.js
+++ Blackjack.js
@@ -1,25 +1,35 @@
-var card_array=["img/01.BMP","img/02.BMP","img/03.BMP","img/04.BMP","img/05.BMP","img/06.BMP","img/07.BMP",
-"img/08.BMP","img/09.BMP","img/10.BMP","img/11.BMP","img/11.BMP","img/12.BMP","img/13.BMP","img/14.BMP","img/15.BMP",
-"img/16.BMP","img/17.BMP","img/18.BMP","img/19.BMP","img/20.BMP","img/21.BMP","img/22.BMP","img/23.BMP","img/24.BMP",
-"img/25.BMP","img/26.BMP","img/27.BMP","img/28.BMP","img/29.BMP","img/30.BMP","img/31.BMP","img/32.BMP","img/33.BMP",
-"img/34.BMP","img/35.BMP","img/36.BMP","img/37.BMP","img/38.BMP","img/39.BMP","img/40.BMP","img/41.BMP","img/42.BMP",
-"img/43.BMP","img/44.BMP","img/45.BMP","img/46.BMP","img/47.BMP","img/48.BMP","img/49.BMP","img/51.BMP","img/52.BMP"];
+var card_list = []
+card_list[0] = "never_used";
+card_list[1] = "place_holder"
+card_list[2] = ["img/02.BMP","img/15.BMP","img/28.BMP","img/41.BMP"]; // pierre x
+card_list[3] = ["img/03.BMP","img/16.BMP","img/29.BMP","img/42.BMP"]; // fiole x
+card_list[4] = ["img/04.BMP","img/17.BMP","img/30.BMP","img/43.BMP"]; // feu x
+card_list[5] = ["img/05.BMP","img/18.BMP","img/31.BMP","img/44.BMP"]; // couteau x
+card_list[6] = ["img/06.BMP","img/19.BMP","img/32.BMP","img/45.BMP"]; // saix
+card_list[7] = ["img/07.BMP","img/20.BMP","img/33.BMP","img/46.BMP"]; // epee x
+card_list[8] = ["img/08.BMP","img/21.BMP","img/34.BMP","img/47.BMP"]; // hache x
+card_list[9] = ["img/09.BMP","img/22.BMP","img/35.BMP","img/48.BMP"]; // lance
+card_list[10] = ["img/10.BMP","img/23.BMP","img/36.BMP","img/49.BMP", // guandao x
+"img/11.BMP","img/24.BMP","img/37.BMP","img/50.BMP",
+"img/12.BMP","img/25.BMP","img/38.BMP","img/51.BMP",
+"img/13.BMP","img/26.BMP","img/39.BMP","img/52.BMP"];
+card_list[11] = ["img/01.BMP","img/14.BMP","img/27.BMP","img/40.BMP"]; // kamehameha 
 
-var score_array=[1,2,3,4,5,6,7,8,9,10,10,10,10,
-    1,2,3,4,5,6,7,8,9,10,10,10,10,
-    1,2,3,4,5,6,7,8,9,10,10,10,10,
-    1,2,3,4,5,6,7,8,9,10,10,10,10];
 
+var index_point = 0;
+var index_card = 0;
 var index = 0;
 var player_score = 0;
 var bank_score = 0;
 var score = 0;
+var score_list_player = [];
+var score_list_bank = [];
 
 //ELEMENTS HTML
 var divJS = document.getElementById('div2');
 var player_card = document.getElementById("player_card");
 var bank_card = document.getElementById("bank_card");
-var updated = document.getElementById("Modified")
+var updated = document.getElementById("Modified");
 var updated2 = document.getElementById("Modified_bank");
 
 
@@ -30,75 +40,93 @@ function creerImg(chemin) {
 }
 
 
-function Pick_a_card(index){
-    var new_card=card_array[index];
-    while (new_card == "picked"){
-        index = Math.floor((Math.random() * 52));
-        new_card=card_array[index];
+function Pick_a_card(){
+    index_point = Math.floor((Math.random()* 10)+2); // Recupere un nombre entre 2 et 11
+    if (index_point==10){
+        index_card = Math.floor((Math.random()*16));
     }
-    card_array[index] = "picked";
+    else{
+        index_card = Math.floor((Math.random()*4));
+    }
+    var new_card=card_list[index_point][index_card];
+    while (new_card == "picked"){
+        index_point = Math.floor((Math.random()* 10)+2); // Recupere un nombre entre 2 et 11
+        if (index_point==10){
+            index_card = Math.floor((Math.random()*16));
+        }
+        else{
+            index_card = Math.floor((Math.random()*4));
+        }
+        new_card=card_list[index_point][index_card];
+    }
+    card_list[index_point][index_card] = "picked";
     return new_card
 }
 
 
 function getPlayerCard() {
-    index = Math.floor((Math.random() * 52)); // Recupere un nombre entre 0 et 52
-    var player = Pick_a_card(index)
+    var player = Pick_a_card();
     var newImg = creerImg(player);
     divJS.appendChild(newImg);
-    player_score = Player_count(index,player_score,player);
-    if (player_score > 21){
-        updated.textContent = "";
+    player_score = Player_count(score_list_player);
+    if (player_score > 42){
+        updated.textContent = "Vous avez "+ player_score;
         updated2.textContent = "Vous avez perdu.";
     }
     else {
         updated.textContent = "Vous avez "+ player_score +". Carte ou Reste?";
-
+        
     }
     
 }   
 
 function getBankCard(){
-    for (var i=0; i<4; i++) {
-        var index2 = Math.floor((Math.random() * 52)); // Recupere un nombre entre 0 et 52
-        var bank = Pick_a_card(index2);    
+    for (var i=0; i<7; i++) {
+        var bank = Pick_a_card();    
         var newImg = creerImg(bank);
-        var divJS = document.getElementById('div1');
+        divJS = document.getElementById('div1');
         divJS.appendChild(newImg);
-        bank_score = Player_count(index2,bank_score,bank);
-        if (bank_score > 21){
+        bank_score = Player_count(score_list_bank);
+        if (bank_score > 42){
             updated2.textContent = "Vous avez gagne.";
             updated.textContent = "";
             break
             
         }
-        else if (bank_score<21 & bank_score>player_score) {
-            updated2.textContent = "Vous avez perdu"
-            updated.textContent ="";   
+        else if (bank_score<42 & bank_score>player_score) {
+            updated2.textContent = "Vous avez perdu";
+            updated.textContent ="Vous avez "+ player_score;   
             break    
         }
         else {
             updated2.textContent = "La banque a "+ bank_score;
         }
-    if (bank_score < player_score){
-        updated2.textContent = "Vous avez gagne.";
-        updated.textContent = "";
-    }
-    if (bank_score == player_score){
-        updated2.textContent = "Egalite";
-        updated.textContent = "";  
-    }    
+        if (bank_score < player_score){
+            updated2.textContent = "Vous avez gagne.";
+            updated.textContent = "";
+        }
+        if (bank_score == player_score){
+            updated2.textContent = "Egalite";
+            updated.textContent = "";  
+        }    
     }   
 }
 
-function Player_count(index,scored,card){
-    if (card == "img/01.BMP" || card == "14.BMP" || card == "27.BMP" || card =="40.BMP"){
-        var add_score = 1
+function Player_count(scored){
+    var new_score = 0;
+    scored.push(index_point);
+    for (var i=0;i<scored.length;i++){
+        new_score = scored[i] + new_score;
     }
-    else {
-        var add_score = score_array[index-1];
-    }
-    new_score = scored + add_score ;
+    console.log(scored)
+    if (new_score>42){
+        for (var j=0;j<scored.length;j++){
+            if (scored[j] == 11){   
+                scored[j] = 1;
+            }
+        }
+        new_score = scored[i] + new_score;
+    } 
     return new_score
 }
 
